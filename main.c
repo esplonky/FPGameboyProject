0bb449d183c37163dc44b6b0c3a5ab1e5a7ed719
Forrest Hodges <esplonky@gvtc.com>
2020-01-27 16:00:08 -0600

    4-tile sprites, better movement, GameCharacter bugfix, performantdelay()
    Added movegamecharacter() to utilize 4-tile sprites, fixed the compile breaking bug with the two structs for game characters. Added performantdelay()

--- main.c
+++ main.c
@@ -1,90 +1,98 @@
 #include <stdio.h>
+// @ts-ignore
 #include <gb/gb.h>
 #include "sprites.c"
 #include "bgs.c"
 #include "GameCharacter.c"
 
-struct GameCharacter frog;
-struct GameCharacter pickle;
+struct GameCharacter frogChar;
+struct GameCharacter pickleChar;
+UBYTE spritesize = 8;
 
+void performantdelay(UINT8 numloops){
+    UINT8 i;
+    for (i=0; i < numloops; i++){
+        wait_vbl_done();
+    }
+}
+
+void movegamecharacter(struct GameCharacter* character, UINT8 x, UINT8 y){
+    move_sprite(character->spritids[0], x, y);
+    move_sprite(character->spritids[1], x + spritesize, y);
+    move_sprite(character->spritids[2], x, spritesize + y);
+    move_sprite(character->spritids[3], x + spritesize, y + spritesize);
+}
 
 void setupPickle(){
-    pickle.x = 80;
-    pickle.y = 130;
-    pickle.width = 16;
-    pickle.height = 16;
+    pickleChar.x = 80;
+    pickleChar.y = 130;
+    pickleChar.width = 16;
+    pickleChar.height = 16;
 
     set_sprite_tile(0,0);
-    pickle.spritids[0] = 0;
+    pickleChar.spritids[0] = 0;
     set_sprite_tile(1,1);
-    pickle.spritids[1] = 1;
+    pickleChar.spritids[1] = 1;
     set_sprite_tile(2,2);
-    pickle.spritids[2] = 2;
+    pickleChar.spritids[2] = 2;
     set_sprite_tile(3,3);
-    pickle.spritids[3] = 3;
+    pickleChar.spritids[3] = 3;
 
-
+    movegamecharacter(&pickleChar, pickleChar.x, pickleChar.y);
 }
 
 
 void setupFrog(){
-frog.x = 30;
-frog.y = 30;
-frog.width = 16;
-frog.height = 16;
+frogChar.x = 30;
+frogChar.y = 30;
+frogChar.width = 16;
+frogChar.height = 16;
 
 set_sprite_tile(0,0);
-frog.spritids[0] = 0;
+frogChar.spritids[0] = 0;
 set_sprite_tile(1,1);
-frog.spritids[1] = 1;
+frogChar.spritids[1] = 1;
 set_sprite_tile(2,2);
-frog.spritids[2] = 2;
+frogChar.spritids[2] = 2;
 set_sprite_tile(3,3);
-frog.spritids[3] = 3;
-
+frogChar.spritids[3] = 3;
+movegamecharacter(&frogChar, frogChar.x, frogChar.y);
 
 }
 
 void main(){
 
-    setupFrog();
+    //setupFrog();
     setupPickle();
+    set_sprite_data(0, 8, pickle);
     set_bkg_data(0,4, bgTiles);
     set_bkg_tiles(0,0,20,18, playfield);
-    move_sprite(0, 88, 78);
-    move_sprite(1,78,88);
     SHOW_BKG;
     SHOW_SPRITES;
     DISPLAY_ON;
     while(1){
-        switch(joypad()){
-            case J_LEFT:
-                scroll_sprite(0,-1,0);
-                break;
-            case J_RIGHT:
-                scroll_sprite(0,1,0);
-                break;
-            case J_UP:
-                scroll_sprite(0,0,-1);
-                break;
-            case J_DOWN:
-                scroll_sprite(0,0,1);
-                break;
-     
-                }   
+                if(joypad() & J_LEFT){
+                    pickleChar.x -= 2;
+                    movegamecharacter(&pickleChar, pickleChar.x, pickleChar.y);
+                }
+                if(joypad() & J_RIGHT){
+                    pickleChar.x += 2;
+                    movegamecharacter(&pickleChar, pickleChar.x, pickleChar.y);
+                }
+                if(joypad() & J_UP){
+                    pickleChar.y -= 2;
+                    movegamecharacter(&pickleChar, pickleChar.x, pickleChar.y);
+                }
+                if(joypad() & J_DOWN){
+                    pickleChar.y += 2;
+                    movegamecharacter(&pickleChar, pickleChar.x, pickleChar.y);
+                }
+                    performantdelay(2);
 
-            /*if (joypad(J_DOWN && J_LEFT)){
-                scroll_sprite(0,-1,1);}
-            if (joypad(J_DOWN && J_RIGHT)){
-                scroll_sprite(0,1,1);}
-            if (joypad(J_UP && J_RIGHT)){
-                scroll_sprite(0,1,-1);}
-            if (joypad(J_UP && J_LEFT)){
-                scroll_sprite(0,-1,-1);}*/
+            }   
+
                
 
-        delay(20);
-
+    
     }
 
-}
